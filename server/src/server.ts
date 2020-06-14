9e890898c535bfa9f4548577babcb4ffc8a6bcff
Willians Paulo <willianspaulo@live.com>
2020-06-13 22:26:47 -0300

    Disponibiliza arquivos na pasta "content" para acessos via URL direto

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import path from 'path';
 import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+//Permite acesso a arquivos estáticos da aplicação. (Para download, visualização ou simplismente disponibilizar o link)
+//http://localhost:3333/download/teste.pdf
+app.use('/download', express.static(path.resolve(__dirname, 'content')))
 
 app.listen(3333);
